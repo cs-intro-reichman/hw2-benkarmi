584c50907a7d988cad787ad5f159d7e2f83fbbf0
benkarmi <69310049+benkarmi@users.noreply.github.com>
2025-11-06 14:27:47 +0200

    COMPLETED

--- Cheers.java
+++ Cheers.java
@@ -13,7 +13,7 @@ public class Cheers {
                         }
                         System.out.println("Give me " + a + " " + letter + ": " + letter + "!");
                 }
-                System.out.println("What does that spell");
+                System.out.println("What does that spell?");
                 for (int i = 0; i < repeat; i++) {
                         System.out.print(word + "!!!");
                 }
