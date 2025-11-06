4504ea1b649ac3d5f3cb8316219eb63b6551fefa
benkarmi <69310049+benkarmi@users.noreply.github.com>
2025-11-06 14:26:31 +0200

    COMPLETED

--- Cheers.java
+++ Cheers.java
@@ -1,13 +1,22 @@
-//feedback
-
-/*
-Great work boaz!
-*/
-
-
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String word = args[0].toUpperCase();
+                int repeat = Integer.parseInt(args[1]);
+                String useAn = "AEFHILMNORSX";
+
+                for (int i = 0; i < word.length(); i++) {
+                        char letter = word.charAt(i);
+                        String a = "a";
+                        if (useAn.indexOf(letter) != -1) {
+                        a = "an";
+                        }
+                        System.out.println("Give me " + a + " " + letter + ": " + letter + "!");
+                }
+                System.out.println("What does that spell");
+                for (int i = 0; i < repeat; i++) {
+                        System.out.print(word + "!!!");
+                }
+
         }
 }
